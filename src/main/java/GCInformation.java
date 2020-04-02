c0b91395c4499494404cca12e4dcc46abf6e14e5
Daniel Tschan <tschan+github@puzzle.ch>
2020-04-02 20:31:12 +0200

    Initial commit

---
+++ src/main/java/GCInformation.java
@@ -0,0 +1,16 @@
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+public class GCInformation {
+
+    public static void main(String[] args) {
+        List<GarbageCollectorMXBean> gcMxBeans = ManagementFactory.getGarbageCollectorMXBeans();
+
+        for (GarbageCollectorMXBean gcMxBean : gcMxBeans) {
+            System.out.println(gcMxBean.getName());
+            System.out.println(gcMxBean.getObjectName());
+        }
+    }
+}
+
